2171add858debe41610ab2ad8e87e2bae8c8b539
woojin97318 <woojin97318@naver.com>
2022-03-20 17:20:30 +0900

    Q11047 풀이완료

--- src/backjoon/Q11047.java
+++ src/backjoon/Q11047.java
@@ -4,8 +4,23 @@ import java.util.*;
 
 public class Q11047 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int cnt = 0;
+        for(int i = arr.length - 1; i >= 0; i--) {
+            if(arr[i] <= k) {
+                cnt += k / arr[i];
+                k %= arr[i];
+            }
+        }
+        
+        System.out.print(cnt);
 	}
 }
